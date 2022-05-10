5b687990f2b0b9a0747d82a2d3f2a2b76eb6b9f4
polymahh <otman.elkantaoui@gmail.com>
2022-05-10 13:11:33 +0100

    added linting to project (es-lint/prettier)

--- src/index.js
+++ src/index.js
@@ -1,166 +1,195 @@
 import './css/skeleton.css';
 import './css/normalize.css';
-import {getTask,  setTask , setProject, getProject } from './taskStorage';
-
-
-
+import {
+  getTask,
+  setTask,
+  setProject,
+  getProject,
+  changeStatus,
+  emptyStorage,
+} from './taskStorage';
 
 (function () {
-
-class Task {
-    constructor (title, discription, dueDate, status){
-        this.title = title;
-        this.discription = discription;
-        this.dueDate = dueDate ;
-        this.status = status
+  class Task {
+    constructor(title, discription, dueDate, status) {
+      this.title = title;
+      this.discription = discription;
+      this.dueDate = dueDate;
+      this.status = status;
     }
-}
-let mappyDo = {
-currentSection :'',
-mySection : [],
+  }
+  const mappyDo = {
+    currentSection: '',
+    mySection: [],
 
+    init() {
+      this.casheDom();
+      this.bindEvents();
+      this.render();
+      this.callStorage('home');
+    },
+    casheDom() {
+      this.homeButton = document.getElementById('home');
+      this.todayButton = document.getElementById('today');
+      this.weeklyButton = document.getElementById('weekly');
+      this.tasks = document.getElementById('tasks');
+      this.addTask = document.getElementById('taskButton');
+      this.taskForm = document.getElementById('taskForm');
+      this.closeTask = document.getElementById('close');
+      this.saveTask = document.getElementById('save');
+      this.addProject = document.getElementById('projectButton');
+      this.projectForm = document.getElementById('projectForm');
+      this.closeProject = document.getElementById('projectClose');
+      this.saveProject = document.getElementById('projectSave');
+      this.sectionTitle = document.getElementById('sectionTitle');
+      // task object
+      this.title = document.getElementById('taskTitle');
+      this.discription = document.getElementById('taskDescription');
+      this.dueDate = document.getElementById('dueDate');
+      // this.status = document.getElementById("")
+      // project
 
+      this.projectTitle = document.getElementById('projectTitle');
+      this.projectList = document.getElementById('projectList');
+      this.dStorage = document.getElementById('dStorage');
 
-init (){
-    this.casheDom();
-    this.bindEvents();
-    this.render();
-    this.callStorage('home')
-},
-casheDom (){
-    this.homeButton = document.getElementById('home') 
-    this.todayButton = document.getElementById('today') 
-    this.weeklyButton = document.getElementById('weekly')
-    this.tasks = document.getElementById('tasks')
-    this.addTask = document.getElementById('taskButton')
-    this.taskForm = document.getElementById('taskForm')
-    this.closeTask = document.getElementById('close')
-    this.saveTask = document.getElementById('save')
-    this.addProject = document.getElementById('projectButton')
-    this.projectForm = document.getElementById('projectForm')
-    this.closeProject = document.getElementById('projectClose')
-    this.saveProject = document.getElementById('projectSave')
-    this.sectionTitle = document.getElementById('sectionTitle')
-    //task object
-    this.title = document.getElementById("taskTitle")
-    this.discription = document.getElementById("taskDescription")
-    this.dueDate = document.getElementById("dueDate")
-    // this.status = document.getElementById("")
-    //project 
+      // // sliding menu
+      this.menuButton = document.getElementById('dropDown');
+      this.slidingMenu = document.getElementById('slidingMenu');
+    },
+    bindEvents() {
+      this.homeButton.addEventListener('click', () => this.callStorage('home'));
+      this.todayButton.addEventListener('click', () =>
+        this.callStorage('today')
+      );
+      this.weeklyButton.addEventListener('click', () =>
+        this.callStorage('weekly')
+      );
+      this.addTask.addEventListener('click', () => this.showTaskForm());
+      this.closeTask.addEventListener('click', (e) => this.hideTaskForm(e));
+      this.saveTask.addEventListener('click', (e) => this.hideTaskForm(e));
+      this.addProject.addEventListener('click', () => this.showProjectForm());
+      this.closeProject.addEventListener('click', (e) =>
+        this.hideProjectForm(e)
+      );
+      this.saveProject.addEventListener('click', (e) =>
+        this.hideProjectForm(e)
+      );
+      this.projectList.addEventListener('click', (e) =>
+        this.callStorage(e.target.id)
+      );
+      this.dStorage.addEventListener('click', () => emptyStorage());
+      this.tasks.addEventListener('click', (e) => this.showDetails(e.target));
+      this.menuButton.addEventListener('mouseover', () => {
+        this.slidingMenu.style.transform = 'scale(1,1)';
+      });
+      this.menuButton.addEventListener('mouseout', () => {
+        this.slidingMenu.style.transform = 'scale(1,0)';
+      });
+    },
+    render() {
+      this.tasks.innerHTML = '';
+      this.projectList.innerHTML = '';
+      const projects = getProject();
+      if (projects.length > 0) {
+        projects.forEach((project) => this.showProject(project));
+      }
+      this.mySection.forEach((element) => this.showTask(element));
+    },
+    callStorage(section) {
+      // with this we know what project we are in
+      this.currentSection = section;
+      this.mySection = getTask(section);
+      this.sectionTitle.innerHTML = section.toUpperCase();
+      this.render();
+    },
+    showTask(task) {
+      const taskContainer = document.createElement('div');
+      taskContainer.id = task.title;
 
-    this.projectTitle = document.getElementById("projectTitle")
-    this.projectList = document.getElementById("projectList")
+      const taskTitle = document.createElement('div');
+      taskTitle.textContent = task.title;
+      const taskDueDate = document.createElement('div');
+      taskDueDate.textContent = task.dueDate;
+      const taskStatus = document.createElement('div');
+      taskStatus.textContent = task.status;
+      const taskdelete = document.createElement('input');
+      taskdelete.setAttribute('type', 'checkbox');
+      taskdelete.checked = task.status;
+      if (taskdelete.checked === false) {
+        taskContainer.className = 'taskbox';
+      } else taskContainer.className = 'taskbox done';
+      taskdelete.id = 'done';
 
+      taskdelete.addEventListener('change', (e) => {
+        if (e.target.checked === false) {
+          taskContainer.className = 'taskbox';
+          changeStatus(task.title, false, this.currentSection);
+        } else if (e.target.checked === true) {
+          taskContainer.className = 'taskbox done';
+          changeStatus(task.title, true, this.currentSection);
+        } else console.log('something is wrong');
+      });
 
+      taskContainer.append(taskTitle, taskDueDate, taskStatus, taskdelete);
+      this.tasks.appendChild(taskContainer);
+    },
+    showDetails(traget) {
+      console.log(traget);
+    },
+    showProject(project) {
+      const projectContainer = document.createElement('div');
+      projectContainer.innerHTML = project;
+      projectContainer.setAttribute('id', project);
+      projectContainer.setAttribute('class', 'button');
 
-},
-bindEvents (){
-    this.homeButton.addEventListener('click', () => this.callStorage('home'));
-    this.todayButton.addEventListener('click', () => this.callStorage('today'));
-    this.weeklyButton.addEventListener('click', () => this.callStorage('weekly'));
-    this.addTask.addEventListener('click', () => this.showTaskForm());
-    this.closeTask.addEventListener('click', (e) => this.hideTaskForm(e));
-    this.saveTask.addEventListener('click', (e) => this.hideTaskForm(e));
-    this.addProject.addEventListener('click', () => this.showProjectForm());
-    this.closeProject.addEventListener('click', (e) => this.hideProjectForm(e));
-    this.saveProject.addEventListener('click', (e) => this.hideProjectForm(e));
-    this.projectList.addEventListener('click', (e) => this.callStorage(e.target.id));
-    this.tasks.addEventListener('click', (e) => {
-        
-        if(e.target.matches("#done")){
-            this.taskDone(e)
-        }else console.log('dis'+ e)
-    }
-    );
+      this.projectList.appendChild(projectContainer);
+    },
+    showTaskForm() {
+      this.taskForm.style.display = 'block';
+    },
+    hideTaskForm(e) {
+      if (e.target.id === 'close') {
+        console.log('close');
+        this.taskForm.style.display = 'none';
+      } else if (e.target.id === 'save') {
+        console.log('save');
+        const newTask = new Task(
+          this.title.value,
+          this.discription.value,
+          this.dueDate.value,
+          ''
+        );
+        setTask(this.currentSection, newTask);
+        this.taskForm.style.display = 'none';
+        this.callStorage(this.currentSection);
+      }
+    },
+    showProjectForm() {
+      this.projectForm.style.display = 'inline';
+    },
+    hideProjectForm(e) {
+      if (e.target.id === 'projectClose') {
+        console.log('close');
+        this.projectForm.style.display = 'none';
+      } else if (e.target.id === 'projectSave') {
+        if (this.projectTitle.value !== '') {
+          console.log('save');
+          setProject(this.projectTitle.value);
+          this.projectForm.style.display = 'none';
+          this.projectTitle.value = '';
+          this.callStorage(this.currentSection);
+        } else {
+          this.projectForm.style.display = 'none';
+        }
+      }
+    },
+    taskDiscripton(task) {
+      console.log('discription');
+    },
+  };
 
-},
-render (){
-    this.tasks.innerHTML  = '';
-    this.projectList.innerHTML ="";
-    let projects = getProject();
-    if (projects.length > 0){
-        projects.forEach( (project) => this.showProject(project))
-    }
-    this.mySection.forEach(element => this.showTask(element))
-},
-callStorage(section) {
-    // with this we know what project we are in 
-    this.currentSection = section;
-    this.mySection = getTask(section);
-    this.sectionTitle.innerHTML = section.toUpperCase();
-    this.render();
-},
-showTask (task) {
-    const taskContainer = document.createElement('tr');
-    taskContainer.id = task.title;
-    const taskTitle = document.createElement('td');
-    taskTitle.textContent = task.title
-    const taskDueDate = document.createElement('td');
-    taskDueDate.textContent = task.dueDate
-    const taskStatus = document.createElement('td');
-    taskStatus.textContent = task.status
-    const taskdelete = document.createElement('td');
-    taskdelete.innerHTML = `<input type="checkbox">`
-    taskdelete.id = 'done'
-    
-    taskContainer.append(taskTitle,taskDueDate,taskStatus, taskdelete)
-    this.tasks.appendChild(taskContainer);
-},
-showProject (project){
-    const projectContainer = document.createElement('div');
-    projectContainer.innerHTML=project;
-    projectContainer.setAttribute("id", project)
-    projectContainer.setAttribute("class", "button")
-    
-    this.projectList.appendChild(projectContainer)
-},
-showTaskForm (){
-    this.taskForm.style.display = "block";
-},
-hideTaskForm (e){
-    if (e.target.id == "close"){
-        console.log('close')
-        this.taskForm.style.display = "none";
-    }else if (e.target.id == "save"){
-        console.log("save")
-        let newTask = new Task(this.title.value, this.discription.value, this.dueDate.value, "")
-        setTask(this.currentSection, newTask)
-        this.taskForm.style.display = "none";
-        this.callStorage(this.currentSection)
-    }
-},
-showProjectForm () {
-    this.projectForm.style.display = "inline";
-},
-hideProjectForm(e){
-    if (e.target.id == "projectClose"){
-        console.log('close')
-        this.projectForm.style.display = "none";
-    }else if (e.target.id == "projectSave"){
-       if (this.projectTitle.value != ''){
-            console.log("save")
-            setProject(this.projectTitle.value)
-            this.projectForm.style.display = "none";
-            this.projectTitle.value = '';
-            this.callStorage(this.currentSection)
-       }else {
-            this.projectForm.style.display = "none";
-       }
-        
-    }
-},
-taskDone(task){
-    console.log(task)
-
-},
-taskDiscripton(task){
-    console.log('discription')
-},
-
-
-}
-
-mappyDo.init();
-})()
+  mappyDo.init();
+})();
 
 console.log('working');
--- src/taskStorage.js
+++ src/taskStorage.js
@@ -1,114 +1,177 @@
 let projects = [];
 
-let projectArrays = {};
-const home =[
-    {
-        title: 'ffff',
-        discription: 'ffff',
-        dueDate: 'ffff',
-        status: false,
-    },
-    {
-        title: 'ffff',
-        discription: 'ffff',
-        dueDate: 'ffff',
-        status: false,
-    },
-    {
-        title: 'ffff',
-        discription: 'ffff',
-        dueDate: 'ffff',
-        status: false,
-    }
-]
-const today =[
-    {
-        title: 'ddd',
-        discription: 'ddd',
-        dueDate: 'ddd',
-        status: false,
-    },
-    {
-        title: 'ddd',
-        discription: 'ddd',
-        dueDate: 'ddd',
-        status: false,
-    },
-    {
-        title: 'ddd',
-        discription: 'ddd',
-        dueDate: 'ddd',
-        status: false,
-    }
-]
-const weekly =[
-    {
-        title: 'dddfff',
-        discription: 'dddfff',
-        dueDate: 'dddfff',
-        status: false,
-    },
-    {
-        title: 'dddfff',
-        discription: 'dddfff',
-        dueDate: 'dddfff',
-        status: false,
-    },
-    {
-        title: 'dddfff',
-        discription: 'dddfff',
-        dueDate: 'dddfff',
-        status: false,
-    }
-]
+const projectArrays = {};
+let home = [
+  {
+    title: 'ffff1',
+    discription: 'ffff',
+    dueDate: 'ffff',
+    status: false,
+  },
+  {
+    title: 'ffff2',
+    discription: 'ffff',
+    dueDate: 'ffff',
+    status: true,
+  },
+  {
+    title: 'ffff3S',
+    discription: 'ffff',
+    dueDate: 'ffff',
+    status: false,
+  },
+];
+let today = [
+  {
+    title: 'ddd',
+    discription: 'ddd',
+    dueDate: 'ddd',
+    status: false,
+  },
+  {
+    title: 'ddd',
+    discription: 'ddd',
+    dueDate: 'ddd',
+    status: false,
+  },
+  {
+    title: 'ddd',
+    discription: 'ddd',
+    dueDate: 'ddd',
+    status: false,
+  },
+];
+let weekly = [
+  {
+    title: 'dddfff',
+    discription: 'dddfff',
+    dueDate: 'dddfff',
+    status: false,
+  },
+  {
+    title: 'dddfff',
+    discription: 'dddfff',
+    dueDate: 'dddfff',
+    status: false,
+  },
+  {
+    title: 'dddfff',
+    discription: 'dddfff',
+    dueDate: 'dddfff',
+    status: false,
+  },
+];
 
-const getTask = (section )=> {
-    if(section == 'home'){
-        return home;
-        
-    }
-    else if(section == 'today'){
-        return today;
-    }
-    else if (section == 'weekly') {
-        return weekly;
-    }
-    else if (projects.includes(section)){
-        return projectArrays[section]
-    }
-    else{
-        console.log('non selected');
-    }
+const getTask = (section) => {
+  let result;
+  if (section === 'home') {
+    result = home;
+  } else if (section === 'today') {
+    result = today;
+  } else if (section === 'weekly') {
+    result = weekly;
+  } else if (projects.includes(section)) {
+    result = projectArrays[section];
+  } else {
+    console.log('non selected');
+  }
+  return result;
 };
 
 const setTask = (section, obj) => {
-    if(section == 'home'){
-        home.push(obj)   
-    }
-    else if(section == 'today'){
-         today.push(obj) 
-    }
-    else if (section == 'weekly') {
-         weekly.push(obj) 
-    }
-    else if (projects.includes(section)){
-        projectArrays[section].push(obj)
-    }
-    else{
-        console.log('non selected');
-    }
-} 
+  if (section === 'home') {
+    home.push(obj);
+  } else if (section === 'today') {
+    today.push(obj);
+  } else if (section === 'weekly') {
+    weekly.push(obj);
+  } else if (projects.includes(section)) {
+    projectArrays[section].push(obj);
+  } else {
+    console.log('non selected');
+  }
+};
+
+/* eslint-disable no-param-reassign */
+const changeStatus = (title, state, section) => {
+  if (section === 'home') {
+    home.forEach((task) => {
+      if (task.title === title) {
+        task.status = state;
+      }
+    });
+  } else if (section === 'today') {
+    today.forEach((task) => {
+      if (task.title === title) {
+        task.status = state;
+      }
+    });
+  } else if (section === 'weekly') {
+    weekly.forEach((task) => {
+      if (task.title === title) {
+        task.status = state;
+      }
+    });
+  } else if (projects.includes(section)) {
+    projectArrays[section].forEach((task) => {
+      if (task.title === title) {
+        task.status = state;
+      }
+    });
+  } else {
+    console.log('non selected');
+  }
+};
+/* eslint-disable no-param-reassign */
 
 const setProject = (section) => {
-    projects.push(section);
-    projectArrays[section]=[]
-    console.log(projects)
-    console.log(projectArrays)
+  projects.push(section);
+  projectArrays[section] = [];
+  console.log(projects);
+  console.log(projectArrays);
+  // eslint-disable-next-line no-use-before-define
+  updateLocalStorage();
+};
+
+const getProject = () => projects;
+
+function updateLocalStorage() {
+  localStorage.setItem('home', JSON.stringify(home));
+  localStorage.setItem('today', JSON.stringify(today));
+  localStorage.setItem('weekly', JSON.stringify(weekly));
+  localStorage.setItem('projects', JSON.stringify(projects));
+  if (projects.length > 0) {
+    projects.forEach((element) => {
+      localStorage.setItem(element, JSON.stringify(projectArrays[element]));
+    });
+  }
 }
+const checkLocalStorage = () => {
+  if (localStorage.getItem('home')) {
+    home = JSON.parse(localStorage.getItem('home'));
+  }
+  if (localStorage.getItem('today')) {
+    today = JSON.parse(localStorage.getItem('today'));
+  }
+  if (localStorage.getItem('weekly')) {
+    weekly = JSON.parse(localStorage.getItem('weekly'));
+  }
+  if (localStorage.getItem('projects')) {
+    projects = JSON.parse(localStorage.getItem('projects'));
+    projects.forEach((element) => {
+      projectArrays[element] = JSON.parse(localStorage.getItem(element));
+    });
+  }
+};
+checkLocalStorage();
+const emptyStorage = () => {
+  localStorage.removeItem('home');
+  localStorage.removeItem('today');
+  localStorage.removeItem('weekly');
+  localStorage.removeItem('projects');
+  projects.forEach((element) => {
+    localStorage.removeItem(element);
+  });
+};
 
-const getProject = () => {
-    return projects
-}
-
-
-export { getTask, setTask, setProject, getProject}
+export { getTask, setTask, setProject, getProject, changeStatus, emptyStorage };
